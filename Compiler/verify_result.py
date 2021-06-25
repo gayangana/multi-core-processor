150a528db58702d29257a120ee2aed54b16b226b
Kavinda Dulhan <kavindadulhan@gmail.com>
2021-06-25 22:12:27 +0530

    Update the verify code

--- Compiler/verify_result.py
+++ Compiler/verify_result.py
@@ -1,20 +1,23 @@
-try:
-    python_result = open("python_result_matrix.txt","r")
-    python_result_file = list(map(int,python_result.readlines()))
-    python_result.close()
-    print("Python result:", python_result_file)
+# try:
+python_result = open("python_result_matrix.txt","r")
+python_result_file = list(map(int,python_result.readlines()))
+python_result.close()
+print("Python result:", python_result_file)
 
-    processor_result = open("processor_result_matrix.txt","r")
-    D1 = int(processor_result.readline().strip())
-    D2 = int(processor_result.readline().strip())
-    D3 = int(processor_result.readline().strip())
-    processor_result_file = list(map(int,processor_result.readlines()))
-    processor_result.close()
-    print("processor_result_file:",processor_result_file[9+D1*D2+D2*D3:])
+processor_result = open("processor_result_matrix.txt","r")
+processor_result.readline().strip()
+D1 = int(processor_result.readline().strip())
+D2 = int(processor_result.readline().strip())
+D3 = int(processor_result.readline().strip())
+processor_result_file = processor_result.readlines()
+processor_result_matrix = processor_result_file[9+D1*D2+D2*D3:9+D1*D2+D2*D3+D1*D3]
+processor_result_matrix = list(map(int,processor_result_matrix))
+processor_result.close()
+print("processor_result_file:",processor_result_matrix)
 
-    if (python_result_file == processor_result_file[9+D1*D2+D2*D3:]):
-        print("Matrix result is correct")
-    else:
-        print("Matrix result is incorrect")
-except:
-    print("File Error")
+if (python_result_file == processor_result_matrix):
+    print("Matrix result is correct")
+else:
+    print("Matrix result is incorrect")
+# except:
+#     print("File Error")
