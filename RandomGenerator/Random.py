0ce946ed9eb0360ec18490c0454836abf2d0fd94
Tharaka Rehan <tharakarehan10@gmail.com>
2021-06-29 14:42:44 +0530

    added support for multi-core implementation

--- RandomGenerator/Random.py
+++ RandomGenerator/Random.py
@@ -1,14 +1,15 @@
 import random
 
+n  = int(input('Enter Number of Cores : '))
 d1 = int(input('Enter Dimension 01 : '))
 d2 = int(input('Enter Dimension 02 : '))
 d3 = int(input('Enter Dimension 03 : '))
+r  = int(input('Enter Range : ')) 
 
-
-MatrixA = [random.choices(population = range(0, 1001), k = d2) for i in range(d1)]
+MatrixA = [random.choices(population = range(0, r+1), k = d2) for i in range(d1)]
 print('MatrixA created')
 
-MatrixB = [random.choices(population=range(0, 1001), k=d3) for i in range(d2)]
+MatrixB = [random.choices(population=range(0, r+1), k=d3) for i in range(d2)]
 print('MatrixB created')
 
 MatrixC = [0] * (d1 * d3)
@@ -29,15 +30,15 @@ for j in result: MatrixC1 += j
 print(MatrixC1)
 MatrixC1 = list(map(str,MatrixC1))
 
-Total = [d1, d2, d3, 12 + d1 * d2, 12 + d1 * d2 + d2 * d3, 12 + d1 * d2 + d2 * d3 + d1 * d3, 0, 0, 0, 0, 0, 0] + MatrixA1 + MatrixB1 + MatrixC
+Total = [n, d1, d2, d3, 8,8 + d1 * d2, 8 + d1 * d2 + d2 * d3, 8 + d1 * d2 + d2 * d3 + d1 * d3] + MatrixA1 + MatrixB1 + MatrixC + [0]*(4*n)
 Total = list(map(str,Total))
 
-with open('RandomGenerator/memory_input.txt', 'w') as f:
+with open('memory_input.txt', 'w') as f:
     for line in Total:
         f.write(line)
         f.write('\n')
 
-with open('RandomGenerator/python_matC.txt', 'w') as f:
+with open('python_matC.txt', 'w') as f:
     for line in MatrixC1:
         f.write(line)
         f.write('\n')
