c842429072b98aa3f7c352911983545ca2abd448
Gayangana Leelarathne <gayangana97@gmail.com>
2021-07-02 21:07:39 +0530

    Add main module for processor control

--- top_tb.v
+++ top_tb.v
@@ -3,12 +3,14 @@
 module top_tb();
 
     reg clk;
-    reg [1:0]status;
-    reg [15:0]com_data_in;
-    reg [15:0]com_addr;
-    reg com_wr_en;
-    wire end_p;
+    reg [15:0]com_data_in;	
+	reg data_write_start;
+	reg data_write_done;
+   
     wire [15:0]com_data_out;
+    wire [1:0]state;
+    wire output_write_done;
+    wire output_write_start;
 
 
     integer data_file;
@@ -17,53 +19,47 @@ module top_tb();
 
 
     initial begin
-        clk = 1'b0;
-        status = 2'b00;
-        //com_data_in = 16'b0;
-        com_addr = 16'd65535;
-        com_wr_en = 1'b1;
+        clk = 1'b0;	
+		data_write_start = 1'b1;
+		data_write_done = 1'b0;
         data_file =$fopen("memory.txt","r");  
-        out_file = $fopen("src/out.txt","w"); 
+        out_file = $fopen("src/out.txt","w");  
+		
     end
 
     always #(5) clk <= ~clk;   
 
-    always @(posedge clk) begin
-        if (end_p==1'b1)
-            status <= 2'b10;
-            com_addr <= 16'd0;
-    end
 		
     always @(posedge clk) begin
-        case (status)
-            2'b00:begin
-                scan_file = $fscanf(data_file, "%d\n", com_data_in); 
-                if (!$feof(data_file)) begin 
-                    com_addr <= com_addr + 1'b1;
-                end
-                else begin
-                    com_addr <= com_addr +1'b1;
-                    status <= 2'b01;	 
-                    com_wr_en <= 1'b0 ;
-                end
+        if (output_write_start ==1'b0)begin
+            scan_file = $fscanf(data_file, "%d\n", com_data_in); 
+            if ($feof(data_file)) begin 
+                data_write_done <= 1'b1;
+                data_write_start <= 1'b0;
             end
+        end
+    
 
-            2'b10: begin
-                com_addr <= com_addr +1'b1;
-                $fwrite(out_file,"%d\n",com_data_out);
+        else begin
+            $fwrite(out_file,"%d\n",com_data_out);
+            if (output_write_done)begin
+                $fclose(data_file);
+                $fclose(out_file);
+                $stop;
             end
-        endcase
+        end
     end
 		
-	top top1(	  
+	main main1(	  
 	
 	.clk(clk),
-    .status(status),
     .com_data_in(com_data_in),
-    .com_addr(com_addr),
-    .com_wr_en(com_wr_en),
-	.end_process(end_p),
-    .com_data_out(com_data_out)
+    .data_write_start(data_write_start),
+    .data_write_done(data_write_done),
+	.state(state),
+    .com_data_out(com_data_out),
+    .output_write_done(output_write_done),
+    .output_write_start(output_write_start)
     
 	);
 
