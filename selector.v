37034aa245c07d4e14f2eec3fe284ca9de5ff83d
JanithGan <janithgan@gmail.com>
2021-07-03 23:53:30 +0530

    Add loops for multi-core generation

--- selector.v
+++ selector.v
@@ -1,23 +1,18 @@
-module selector(
-    input clk,
-    input [1:0]status,
-    input [15:0]com_data_in,
-    input [15:0]com_addr,
-    input com_wr_en,
-    input [15:0]proc_bus,
-    input [15:0]proc_addr,
-    input proc_wr_en,
-    input [15:0]DM_out,
-
-    output reg [15:0]DM_data_in,
-    output reg [15:0]DM_addr,
-    output reg DM_write_en,
-    output reg [15:0]com_data_out,
-    output reg [15:0]proc_data_out
-);
-
-
-
+module selector(input clk,
+                input [1:0]status,
+                input [15:0]com_data_in,
+                input [15:0]com_addr,
+                input com_wr_en,
+                input [15:0]proc_bus,
+                input [15:0]proc_addr,
+                input proc_wr_en,
+                input [15:0]DM_out,
+                output reg [15:0]DM_data_in,
+                output reg [15:0]DM_addr,
+                output reg DM_write_en,
+                output reg [15:0]com_data_out,
+                output reg [15:0]proc_data_out);
+    
     always @(*)begin
         case(status)
             2'b00: begin
@@ -25,17 +20,17 @@ module selector(
                 DM_addr     = com_addr;
                 DM_write_en = com_wr_en;
             end
-
+            
             2'b01: begin
-                DM_data_in  = proc_bus;
-                DM_addr     = proc_addr;
-                DM_write_en = proc_wr_en;
+                DM_data_in    = proc_bus;
+                DM_addr       = proc_addr;
+                DM_write_en   = proc_wr_en;
                 proc_data_out = DM_out;
             end
-
+            
             2'b10: begin
-                DM_addr     = com_addr;
-                DM_write_en = com_wr_en;
+                DM_addr      = com_addr;
+                DM_write_en  = com_wr_en;
                 com_data_out = DM_out;
             end
         endcase
