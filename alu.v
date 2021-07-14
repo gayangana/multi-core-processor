130893fd507839562ad1498bae494b1e36ca0e5e
JanithGan <janithgan@gmail.com>
2021-07-14 18:44:51 +0530

    Remove ALU clk

--- alu.v
+++ alu.v
@@ -1,26 +1,24 @@
-module ALU (input i_clk,
-           input [15:0] i_in1,
-           input [15:0] i_in2,
-           input [2:0] i_alu_op,
-           output reg [15:0] o_alu_out,
-           output reg o_z);
-    
+module ALU (input [15:0] in1,
+            input [15:0] in2,
+            input [2:0] alu_op,
+            output reg [15:0] alu_out,
+            output reg z);
+
     always @(*) begin
-        case (i_alu_op)
-            3'd1: o_alu_out <= i_in2 + i_in1;
-            3'd2: o_alu_out <= i_in2 - i_in1;
-            3'd3: o_alu_out <= i_in2 * i_in1;
-            3'd4: o_alu_out <= i_in2 / i_in1;
-            3'd5: o_alu_out <= i_in2 % i_in1;
-        
+        case (alu_op)
+            3'd1: alu_out <= in2 + in1;
+            3'd2: alu_out <= in2 - in1;
+            3'd3: alu_out <= in2 * in1;
+            3'd4: alu_out <= in2 / in1;
+            3'd5: alu_out <= in2 % in1;
         endcase
         
-        if (o_alu_out[15]==1'b1) 
-            o_z <= 1;
-        else if (o_alu_out==16'd0)
-            o_z <= 1;
+        if (alu_out[15] == 1'b1)
+            z <= 1;
+        else if (alu_out == 16'd0)
+            z <= 1;
         else
-            o_z <= 0;
+            z <= 0;
     end
-    
+
 endmodule
