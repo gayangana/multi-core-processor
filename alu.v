42302539842866503854748a9fafba367b95e3c5
Janith <janithgan@gmail.com>
2021-07-14 23:43:53 +0530

    Modify z flag

--- alu.v
+++ alu.v
@@ -7,20 +7,17 @@ module ALU (input i_clk,
     
     always @(*) begin
         case (i_alu_op)
-            3'd1: o_alu_out <= i_in1 + i_in2;
-            3'd2: begin
-                if (i_in2 > i_in1)
-                    o_alu_out <= i_in2 - i_in1;
-                else
-                    o_alu_out <= 16'b0;
-            end
-            
-            3'd3: o_alu_out <= i_in1 * i_in2;
+            3'd1: o_alu_out <= i_in2 + i_in1;
+            3'd2: o_alu_out <= i_in2 - i_in1;
+            3'd3: o_alu_out <= i_in2 * i_in1;
             3'd4: o_alu_out <= i_in2 / i_in1;
             3'd5: o_alu_out <= i_in2 % i_in1;
+        
         endcase
         
-        if (o_alu_out == 0)
+        if (o_alu_out[15]==1'b1) 
+            o_z <= 1;
+        else if (o_alu_out==16'd0)
             o_z <= 1;
         else
             o_z <= 0;
